9710933761c1bc6e1a3322b00f1b288441529cca
B2 <[bcd6591@gmail.com]>
2020-02-09 18:14:12 +0100

    various changes

---
+++ src/transcoding.py
@@ -0,0 +1,22 @@
+
+
+
+user_dt_UTC       = 'date_UTC'
+user_target       = 'target'
+user_site_name    = 'name'
+
+user_weather_id        = 'weather_id'
+user_weather_name      = 'name'
+user_physical_quantity = 'physical_quantity'
+user_source            = 'source'
+user_source_observed   = 'observed'
+user_source_forecast   = 'forecast'
+user_value             = 'value'
+user_temperature       = 'temperature'
+user_nebulosity        = 'nebulosity'
+user_wind_speed        = 'wind_speed'
+
+user_latitude  = 'latitude'
+user_longitude = 'longitude'
+
+
