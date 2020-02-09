3187449312f8afe670181cacf6e5bf5a6ee310e5
B2 <[bcd6591@gmail.com]>
2020-02-09 18:17:35 +0100

    various changes

--- src/transcoding.py
+++ src/transcoding.py
@@ -20,3 +20,28 @@ user_latitude  = 'latitude'
 user_longitude = 'longitude'
 
 
+
+
+
+transcoding_weather = {' ' : '-',
+                       'Marseille-Marignane' : 'Marignane',
+                       'Le-Luc'              : 'Le Luc',
+                       'St-'                 : 'Saint-',
+                       'Bordeaux-Merignac'   : 'Bordeaux',
+                       'Caen-Carpiquet'      : 'Caen',
+                       'Clermont-Fd'         : 'Clermont-Ferrand',
+                       'Dijon-Longvic'       : 'Dijon',
+                       'Lille-Lesquin'       : 'Lille',
+                       'Lyon-Saint-Exupery'  : 'Lyon-Satolas',
+                       'Nancy-Ochey'         : 'Nancy-Essey',
+                       'Nantes-Bouguenais'   : 'Nantes',
+                       'Rennes-Saint-Jacques': 'Rennes',
+                       'Strasbourg-Entzheim' : 'Strasbourg',
+                       'Tarbes-Ossun'        : 'Tarbes-Ossuns',
+                       "Dumont-D'Urville"    : 'Trappes',
+                       'Troyes-Barberey'     : 'Troyes',
+                       }
+
+transcoding_sites   = {' ' : '-',
+                       '.' : '_',
+                       }
